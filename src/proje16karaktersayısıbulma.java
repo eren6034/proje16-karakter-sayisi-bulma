d43145187711f63b66cc8c0b2063cf39fa499cc0
ErenCanErdoğan <107648911+eren6034@users.noreply.github.com>
2024-01-14 15:25:51 +0300

    proje16karaktersayısıbulma.java

--- src/proje16karaktersayısıbulma.java
+++ src/proje16karaktersayısıbulma.java
@@ -2,17 +2,17 @@ import java.util.Scanner;
 
 public class proje16karaktersayısıbulma {
     public static void main(String[] args) {
-        // Kullanıcıya gösterilecek metin
-        String metin = "Bu bir örnek metindir.";
+      
+        String metin = "Boşuna Yağıyor Yağmur Birlikte Islanyacağız ki";
 
         System.out.println("Metin: " + metin);
 
-        // Kullanıcıdan bir karakter girmesini iste
+        
         Scanner scanner = new Scanner(System.in);
         System.out.print("Bir karakter girin: ");
         char karakter = scanner.next().charAt(0);
 
-        // Karakterin metin içinde kaç kez geçtiğini hesapla
+       
         int sayac = 0;
         for (int i = 0; i < metin.length(); i++) {
             if (metin.charAt(i) == karakter) {
